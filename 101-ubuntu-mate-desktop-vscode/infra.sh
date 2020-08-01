f7b6dea3cb86428eb6ecfa6613d3b458ba685c06
codemonkeybot <codemonkeybot@users.noreply.github.com>
2020-08-01 12:26:04 -0600

    Change KDE Instructions

--- 101-ubuntu-mate-desktop-vscode/infra.sh
+++ 101-ubuntu-mate-desktop-vscode/infra.sh
@@ -6,13 +6,13 @@ logger -t devvm "Installing KDE Desktop ... $?"
 
 sudo dpkg --configure -a
 
-sudo apt-add-repository -y ppa:kubuntu-ppa/ppa
+sudo apt install -q -y kde-plasma-desktop
+sudo apt install -q -y kde-runtime
+sudo apt install --no-install-recommends -q -y kubuntu-desktop
 
 sudo apt-get -q -y update
 
 sudo apt-get -q -y upgrade
-
-sudo apt-get install -q -y kubuntu-desktop
 
 logger -t devvm "KDE Desktop installed. $?"
 
