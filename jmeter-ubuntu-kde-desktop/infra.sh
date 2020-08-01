c4466ba6c45f8d393bc65333d20ae386ec5c70b0
rcunday <rcunday@avaya.com>
2020-08-01 16:30:39 -0600

    Change KDE Install Operations

--- jmeter-ubuntu-kde-desktop/infra.sh
+++ jmeter-ubuntu-kde-desktop/infra.sh
@@ -4,9 +4,7 @@ logger -t devvm "Install started: $?"
 
 logger -t devvm "Installing  plasma KDE $?"
 
-sudo apt-get install -q -y tasksel
-sudo tasksel install kubuntu-desktop
-
+sudo apt-get install -q -y  kubuntu-desktop
 
 sudo apt-get -q -y update
 sudo apt-get -q -y upgrade
